218953da19177c51ad6260d924df16a66e739e2d
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-11-24 15:20:43 +0300

    298. Storing & Fetching Data with SQLite

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,18 +1,27 @@
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(path.join(dbPath, "places.db"),
+    return sql.openDatabase(path.join(dbPath, 'places.db'),
         onCreate: (db, version) {
       return db.execute(
-          "CREATE TABLE user_places(id TEXT PRİMARY KEY, title TEXT, image TEXT)");
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
     }, version: 1);
-    // Buraya kadar veritabanını kesinlikle açıyoruz, ancak oraya hiçbir şey eklemiyoruz,
-    // bunun yerine sadece veritabanımızı oluşturmak için bu komutu çalıştırıyoruz.
+  }
 
-    await sqlDb.insert(table, data,
-        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    // tablodan verileri almak için ekleme metodu
+    final db = await DBHelper.database();
+    db.insert(table, data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    // static yöntem oldugundan class ismini tekrarlamamız gerekir.
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    // verileri almak istedipimiz tabloyu alır ve tablodaki tüm verileri döndürür.
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
